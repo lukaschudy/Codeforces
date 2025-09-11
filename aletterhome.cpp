4035463c916253f94387361b60f4d57836cf371d
Lukas <lukaschudy@hotmail.com>
2025-09-11 22:33:39 +0800

    gitignore

--- aletterhome.cpp
+++ aletterhome.cpp
@@ -1,26 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int t;
     cin >> t;
-
     while (t--) {
-        int n;
-        long long s;
+        int n, s;
         cin >> n >> s;
-
-        vector<long long> x(n);
-        for (int i = 0; i < n; ++i) {
-            cin >> x[i];
-        }
-
-        long long min_x = *min_element(x.begin(), x.end());
-        long long max_x = *max_element(x.begin(), x.end());
-
-        long long steps = max(abs(s - min_x), abs(s - max_x)) + (max_x - min_x);
-        cout << steps << '\n';
+        vector<int> x(n);
+        for (int i = 0; i < n; i++) cin >> x[i];
+        int ans = min(abs(s - x[0]), abs(s - x.back())) + x.back() - x[0];
+        cout << ans << '\n';
     }
-
     return 0;
 }
