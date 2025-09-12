085939a660bd2bfd92ae6c16593514a32c41878f
Lukas <lukaschudy@hotmail.com>
2025-09-12 17:34:53 +0800

    update

---
+++ againtwentyfive.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    if (n=0){
+        cout << "1";
+    }else{
+        cout << "25";
+
+    }
+
+    return 0;
+}
