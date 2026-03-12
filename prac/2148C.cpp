48a33d27491e9ee77db79a5b7366d4a067719b57
Lukas <lukaschudy@hotmail.com>
2026-03-12 23:59:31 +0100

    update

--- prac/2148C.cpp
+++ prac/2148C.cpp
@@ -1,58 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
     int t;
     cin >> t;
     
-    while(t--) {
-        int n, m;
+    while (t--) {
+        long long n, m;
         cin >> n >> m;
         
-        vector<pair<int, int>> req(n);
-        for(int i = 0; i < n; i++) {
-            cin >> req[i].first >> req[i].second;
-        }
+        long long ans = 0;
+        long long prev_a = 0, prev_b = 0;
         
-
-        sort(req.begin(), req.end());
- 
-        if(req[0].first < abs(req[0].second - 0)) {
-            cout << -1 << "\n";
-            continue;
-        }
-        
-        int points = 0;
-        int curr_side = 0;
-        int curr_time = 0;
-        bool possible = true;
-        
-        for(auto& r : req) {
-            int target_time = r.first;
-            int target_side = r.second;
-            int time_available = target_time - curr_time;
-            int dist = abs(target_side - curr_side);
+        for (int i = 0; i < n; i++) {
+            long long a, b;
+            cin >> a >> b;
             
-            if(dist > time_available || (time_available - dist) % 2 != 0) {
-                possible = false;
-                break;
-            }
+            long long gap = a - prev_a;
+            long long need_parity = (b ^ prev_b);
             
-            int extra = time_available - dist;
-            points += dist + extra;
+            if (gap % 2 == need_parity)
+                ans += gap;
+            else
+                ans += gap - 1;
             
-            curr_side = target_side;
-            curr_time = target_time;
+            prev_a = a;
+            prev_b = b;
         }
         
-        if(possible) {
-            cout << points << "\n";
-        } else {
-            cout << -1 << "\n";
-        }
+        ans += m - prev_a;
+        
+        cout << ans << "\n";
     }
     
     return 0;
