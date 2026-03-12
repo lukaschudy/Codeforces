91f908399ed46a845ae8aaa2a6d57abf80797bbf
Lukas <lukaschudy@hotmail.com>
2026-03-12 23:54:07 +0100

    update

--- prac/2149C.cpp
+++ prac/2149C.cpp
@@ -1,28 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     int t;
     cin >> t;
-    while(t--){
-        int n, k;
-        cin >> n >> k;
-
-        map<int,int> freq;
-        for(int i = 0; i < n; i++){
-            int x; cin >> x;
-            freq[x]++;
+    
+    while(t--) {
+        int n, m;
+        cin >> n >> m;
+        
+        vector<pair<int, int>> req(n);
+        for(int i = 0; i < n; i++) {
+            cin >> req[i].first >> req[i].second;
         }
-        int missing = 0;
-        for(int i = 0; i < k; i++)
-            if(freq[i] == 0) missing++;
 
-        int cnt_k = freq[k];
+        sort(req.begin(), req.end());
 
-        cout << max(missing, cnt_k) << "\n";
+        if(req[0].first < abs(req[0].second - 0)) {
+            cout << -1 << "\n";
+            continue;
+        }
+        
+        int points = 0;
+        int curr_side = 0;
+        int curr_time = 0;
+        bool possible = true;
+        
+        for(auto& r : req) {
+            int target_time = r.first;
+            int target_side = r.second;
+            int time_available = target_time - curr_time;
+            int dist = abs(target_side - curr_side);
+            
+            if(dist > time_available || (time_available - dist) % 2 != 0) {
+                possible = false;
+                break;
+            }
+            
+            int extra = time_available - dist;
+            points += dist + extra; 
+            
+            curr_side = target_side;
+            curr_time = target_time;
+        }
+        
+        if(possible) {
+            cout << points << "\n";
+        } else {
+            cout << -1 << "\n";
+        }
     }
+    
     return 0;
 }
