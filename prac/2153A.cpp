888cda42c8167f4f7b93313f47bb94429a28da4c
Lukas <lukaschudy@hotmail.com>
2026-03-20 22:23:21 +0100

    daily exercise

---
+++ prac/2153A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        set<int> s(a.begin(), a.end());
+        int totalcount = s.size();
+        cout << totalcount << endl;
+    }
+    
+    return 0;
+}
