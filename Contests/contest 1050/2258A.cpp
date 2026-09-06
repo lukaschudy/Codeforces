2559617bbb2c4fb513583e440ae3ae73a6c7ce45
Lukas <lukaschudy@hotmail.com>
2026-09-06 10:05:05 +0200

    codeforces practise

---
+++ Contests/contest 1050/2258A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--){
+        int n; cin >> n;
+        vector<int> vctr(n);
+        for (int i = 0; i < n; i++) cin >> vctr[i];
+
+        if(n % 2 == 1){
+            cout << vctr[0];
+        }
+        else{
+
+        }
+
+
+    }
+
+    return 0;
+}
